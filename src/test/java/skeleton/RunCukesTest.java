d5b35592fb9768721cd45c785f751198d859af02
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD52.dir.svc.accenture.com>
2018-08-02 11:59:45 +0530

    CHANGES @CucumberOptions(plugin = {"json:target/cucumber.json"})

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json"},features="src/test/resources/skeleton")
+//@CucumberOptions(plugin = {"json"},features="src/test/resources/skeleton")
+@CucumberOptions(plugin = {"json:target/cucumber.json"})
 public class RunCukesTest {
 }
